a22cbf887ceecf460c8befde05c19404c98aeed4
Felipe Abad <felipeabadmustillo@gmail.com>
2021-10-25 20:47:17 -0300

    Cambios Grales.
    Cambios en la estructura general de el programa y función "refresca_tablero".

--- Memotest/Memotest.py
+++ Memotest/Memotest.py
@@ -1,35 +1,20 @@
-
-diccionario = {1:"s" , 2:"D" , 3:"s" , 4:"D"}
-
-
-def construccion_tablero(eleccion,tablero):
-    tablero_actualizado = tablero
-    tablero_actualizado[eleccion-1] = diccionario[eleccion]
-
-    return tablero_actualizado
-
-
-
-
-
 def memotest_juego(diccionario_fichas):
+    imagenes = ("s", "D")
+    tablero = [[0, False], [1, False], [0, False], [1, False]]
     contador = 0
     posiciones = diccionario_fichas.keys()
     posiciones = list(posiciones)
     posiciones_provisorias = posiciones
-    
-    
 
-    while contador < 2 :
+    while contador < 2:
         print(posiciones)
         primer_ficha = int(input("Por favor, ingrese el numero de la primer ficha: "))
         posiciones_provisorias = construccion_tablero(primer_ficha, posiciones_provisorias)
 
+        tablero[posicion-1][1] = True
 
         #aca se da el problema, por alguna razon, "posiciones" tiene el mismo valor que "posiciones_provisorias"
-        
-        
-        
+
         print(posiciones_provisorias)
         segunda_ficha = int(input("Por favor, ingrese el numero de la segunda ficha: "))
         posiciones_provisorias = construccion_tablero(segunda_ficha , posiciones_provisorias)
@@ -38,17 +23,26 @@ def memotest_juego(diccionario_fichas):
         if diccionario[primer_ficha] == diccionario[segunda_ficha]:
             contador += 1
             posiciones = posiciones_provisorias
-        else: 
+        else:
             posiciones_provisorias = posiciones
-        
 
+def refresca_tablero(tablero, imagenes):
+    """ Felipe: esta funcion se encarga de printear por pantalla el tablero dado por parametro """
+    tablero_string = "Fichas y Posiciones: "
+    casillero_posicion = 0
 
+    for casillero in tablero:
+        casillero_posicion += 1
 
+        if casillero_posicion % 4 == 1 and casillero_posicion != 1:
+            tablero_string += "/n"
 
-
+        if casillero[1]:
+            tablero_string += " " + imagenes[casillero[0]] + " "
+        else:
+            tablero_string += f" [{casillero_posicion}] "
     
 def main():
     memotest_juego(diccionario)
-
 
 main()
