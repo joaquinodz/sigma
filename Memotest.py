f53cb9237fb58b9237146822056c895f4cb84290
Felipe Abad <felipeabadmustillo@gmail.com>
2021-10-26 21:26:20 -0300

    Correcciones en logica principal
    Implemente la funcion refrescar tablero a la logica principal para poder mostrar por consola el estado de el tablero y mejore un poco la logica para que quede un codigo mas consciso.

--- Memotest.py
+++ Memotest.py
@@ -1,49 +1,35 @@
-
-
 def memotest_juego():
-
-    tablero = [["s", False], ["D", False], ["s", False], ["D", False]] # Acá tengo acomodado el tablero con su posicion (indice, letra y estado del juego). 
-    posiciones = [[1],[2],[3],[4]]
-    posiciones_provisorias = [[1],[2],[3],[4]]
+    """
+    tablero es una lista cuyos elementos representan cada "casillero" de el tablero
+    cada casillero tiene a su vez una lista con dos valores
+    el primer valor representa la imagen que tiene el casillero
+    y el segundo valor representa si esta descubierto o no.
+    """
+    tablero = [["s", False], ["D", False], ["s", False], ["D", False]]
 
     while not finalizar(tablero):
-        
         try:
-        
-            print("Fichas y posiciones: ", posiciones_provisorias)
-            primera_posicion = int(input("1er. Posición: "))
-            imagen_primera_posicion = tablero[primera_posicion - 1][0] # en este caso obtengo la ubicación con su imagen
-            tablero[primera_posicion - 1][1] = True 
-            posiciones_provisorias[primera_posicion - 1] = imagen_primera_posicion
+            refresca_tablero(tablero)
+            primera_posicion = int(input("1er. Posición: ")) - 1
+            tablero[primera_posicion][1] = True
 
-            print("Fichas y posiciones: ", posiciones_provisorias)
-            segunda_posicion = int(input("2da. Posición: "))
-            imagen_segunda_posicion = tablero [segunda_posicion - 1][0] # en este caso obtengo la ubicación con su imagen
-            tablero[segunda_posicion - 1][1] = True
-            posiciones_provisorias[segunda_posicion - 1]= imagen_segunda_posicion
-                
-            print("Fichas y posiciones: ", posiciones_provisorias)
+            refresca_tablero(tablero)
+            segunda_posicion = int(input("2da. Posición: ")) - 1
 
-            if imagen_primera_posicion != imagen_segunda_posicion:
-                posiciones_provisorias = posiciones
-                tablero[primera_posicion - 1][1] = False
-                tablero[segunda_posicion - 1][1] = False
+            if tablero[primera_posicion][0] == tablero[segunda_posicion][0]:
+                tablero[primera_posicion][1] = True
+                tablero[segunda_posicion][1] = True
             else:
-                posiciones = posiciones_provisorias
-        
+                tablero[primera_posicion][1] = False
+
         except ValueError:
             print("Has ingresado un número inválido. Intente nuevamente...")
 
         except IndexError:
             print(f"Debes ingresar un número entre 0 y {len(tablero)}")
-                
-            
-            
-    return tablero
-        
-       
-        
-def refresca_tablero(tablero, imagenes):
+
+
+def refresca_tablero(tablero):
     """ Felipe: esta funcion se encarga de printear por pantalla el tablero dado por parametro """
     tablero_string = "Fichas y Posiciones: "
     casillero_posicion = 0
@@ -55,21 +41,23 @@ def refresca_tablero(tablero, imagenes):
             tablero_string += "/n"
 
         if casillero[1]:
-            tablero_string += " " + imagenes[casillero[0]] + " "
+            tablero_string += "  " + casillero[0] + "  "
         else:
             tablero_string += f" [{casillero_posicion}] "
+
+    print(tablero_string)
 
 def finalizar(tablero):
     """ rodrigo: chequea que todos los valores del tablero sean True, si lo son devuelve True, si no, devuelve False """
     devolver = True
-    
+
     for elemento in tablero:
         if not elemento[1]:
             devolver = False
-            
+
     return devolver
 
-    
+
 def main():
     memotest_juego()
 
