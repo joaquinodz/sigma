450f45cfc50aa3235d72a5abe8fcc894dfc17e4b
Felipe Abad <felipeabadmustillo@gmail.com>
2021-10-29 17:57:37 -0300

    Puesta al dia con el equipo
    Optimización general de el código y discusión con el grupo sobre el código en general.

--- Memotest.py
+++ Memotest.py
@@ -1,13 +1,41 @@
+IMAGEN_FICHA = 0
 ESTADO_FICHA = 1
+CANTIDAD_DE_FICHAS = 4
+FICHAS_POR_FILA = 4
 
+def memotest_juego(tablero):
+    refresca_tablero(tablero)
 
-def analizar_resultados(tablero, primera_posicion, segunda_posicion):
-    if tablero[primera_posicion][0] == tablero[segunda_posicion][0]:
-        tablero[primera_posicion][ESTADO_FICHA] = True
-        tablero[segunda_posicion][ESTADO_FICHA] = True
-        
-    else:
-        tablero[primera_posicion][ESTADO_FICHA] = False
+    while not finalizar(tablero):
+
+        # Solicitamos al usuario la 1° posicion, validamos el valor y mostramos la ficha seleccionada
+        primera_posicion = pedir_datos("1° Posición: ", tablero)
+
+        # Solicitamos al usuario la 2° posicion, validamos el valor y mostramos la ficha seleccionada
+        segunda_posicion = pedir_datos("2° Posición: ", tablero)
+
+        if tablero[primera_posicion][IMAGEN_FICHA] != tablero[segunda_posicion][IMAGEN_FICHA]:
+            tablero[primera_posicion][ESTADO_FICHA] = False
+            tablero[segunda_posicion][ESTADO_FICHA] = False
+            refresca_tablero(tablero)
+
+def refresca_tablero(tablero):
+    """ Felipe: esta funcion se encarga de printear por pantalla el tablero dado por parametro """
+    tablero_string = "Fichas y Posiciones: \n"
+    casillero_posicion = 0
+
+    for casillero in tablero:
+        casillero_posicion += 1
+
+        if casillero_posicion % FICHAS_POR_FILA == 1 and casillero_posicion != 1:
+            tablero_string += "\n"
+
+        if casillero[ESTADO_FICHA]:
+            tablero_string += "  " + casillero[IMAGEN_FICHA] + "  "
+        else:
+            tablero_string += f" [{casillero_posicion}] "
+
+    print(tablero_string)
 
 def pedir_datos(mensaje, tablero):
     """
@@ -16,24 +44,40 @@ def pedir_datos(mensaje, tablero):
     """
     pedir = True
     valor = 0
-    
+
     while (pedir):
         try:
             valor = int(input(mensaje)) - 1
-            
+
             if valor < 0 or valor > len(tablero) - 1:
                 print(f"Debes ingresar un número entre 1 y {len(tablero)}")
-            
-            elif tablero[valor][ESTADO_FICHA] == True:
+
+            elif tablero[valor][ESTADO_FICHA]:
                 print("Esa posición ya se encuentra visible. Elige otra...")
 
             else:
                 pedir = False
-                
+
         except ValueError:
             print("Has ingresado un número inválido. Intente nuevamente...")
 
+    tablero[valor][ESTADO_FICHA] = True
+    refresca_tablero(tablero)
+
     return valor
+
+def finalizar(tablero):
+    """ rodrigo: chequea que todos los valores del tablero sean True, si lo son devuelve True, si no, devuelve False """
+    estado_tablero = True
+    casillero = 0
+
+    while estado_tablero and casillero < len(tablero):
+        if not tablero[casillero][ESTADO_FICHA]:
+            estado_tablero = False
+        else:
+            casillero +=1
+
+    return estado_tablero
 
 def inicializar_tablero(cantidad_de_fichas):
     """Joaquin: crea la estructura del tablero y decide el orden de las fichas"""
@@ -45,7 +89,7 @@ def inicializar_tablero(cantidad_de_fichas):
             Meto las "s" en los casilleros impares y las "D" en los pares
             Parece que en la siguiente etapa vamos a tener que hacer un random
         """
-        
+
         if i % 2 != 0:
             tablero.append(["s", False])
 
@@ -54,67 +98,14 @@ def inicializar_tablero(cantidad_de_fichas):
 
     return tablero
 
-def memotest_juego():
+def main():
     """
     tablero es una lista cuyos elementos representan cada "casillero" de el tablero
     cada casillero tiene a su vez una lista con dos valores
     el primer valor representa la imagen que tiene el casillero
     y el segundo valor representa si esta descubierto o no.
     """
-    tablero = inicializar_tablero(8)
-    
-    while not finalizar(tablero):
-        refresca_tablero(tablero)
-        
-        # Solicitamos al usuario la 1° posicion y validamos el valor
-        primera_posicion = pedir_datos("1° Posición: ", tablero)
+    tablero = inicializar_tablero(CANTIDAD_DE_FICHAS)
+    memotest_juego(tablero)
 
-        # Mostramos que tiene esa posición
-        tablero[primera_posicion][ESTADO_FICHA] = True
-        refresca_tablero(tablero)
-
-        # Solicitamos al usuario la 2° posicion y validamos el valor    
-        segunda_posicion = pedir_datos("2° Posición: ", tablero)
-
-        analizar_resultados(tablero, primera_posicion, segunda_posicion)
-            
-        refresca_tablero(tablero)
-
-def refresca_tablero(tablero):
-    """ Felipe: esta funcion se encarga de printear por pantalla el tablero dado por parametro """
-    tablero_string = "Fichas y Posiciones: \n"
-    casillero_posicion = 0
-    FICHAS_POR_FILA = 4
-    
-    for casillero in tablero:
-        casillero_posicion += 1
-
-        if casillero_posicion % FICHAS_POR_FILA == 1 and casillero_posicion != 1:
-            tablero_string += "\n"
-
-        if casillero[1]:
-            tablero_string += "  " + casillero[0] + "  "
-        else:
-            tablero_string += f" [{casillero_posicion}] "
-
-    print(tablero_string)
-
-def finalizar(tablero):
-    """ rodrigo: chequea que todos los valores del tablero sean True, si lo son devuelve True, si no, devuelve False """
-    devolver = True
-
-    estado_tablero = True
-    listas = 0
-
-    while estado_tablero and listas < len(tablero):
-        if not tablero[listas][ESTADO_FICHA]:
-            devolver = False
-            estado_tablero = False
-        else:
-            listas +=1
-
-    return devolver
-
-def main():
-    memotest_juego()
 main()
