847a6463329017ade24de93b6540a77c74177fe4
Joaquin <jopiodz00@gmail.com>
2021-12-01 18:21:55 -0300

    limpio carga de config

--- manejo_de_archivos.py
+++ manejo_de_archivos.py
@@ -1,3 +1,4 @@
+import constantes
 from constantes import USUARIO,CONTRASENIA, CONFIGURACION_DEFAULT
 
 def leer_archivo(archivo):
@@ -9,7 +10,7 @@ def leer_archivo(archivo):
     if linea:
         linea_procesada = linea.split(',')
     else:
-        linea_procesada =  None, None 
+        linea_procesada = None
 
     return linea_procesada
 
@@ -27,22 +28,23 @@ def convertir_contrasenia_a_diccionario():
 
 def cargar_configuracion():
     """Joaquin: Carga la configuración del juego desde un archivo"""
+    CLAVE = 0
+    VALOR = 1
+
     with open("configuracion.csv", 'r') as archivo_configuracion:
-        configuracion = archivo_configuracion.readlines()
+        lista_de_lineas = archivo_configuracion.readlines()
     
     # Saco el salto de linea al final de cada linea
     # Convierto los valores separados por coma en listas - [clave, valor]
-    configuracion = [linea.rstrip('\n').split(',') for linea in configuracion]
+    lista_de_lineas = [linea.rstrip('\n').split(',') for linea in lista_de_lineas]
     
     # {'nombre_del_parametro': valor_del_parametro}
-    configuracion = {linea[0]:smart_cast(linea[1]) for linea in configuracion}
+    constantes.configuracion = {linea[CLAVE]:smart_cast(linea[VALOR]) for linea in lista_de_lineas}
 
     # Si no hay configuracion, uso la configuracion por defecto
-    for key in CONFIGURACION_DEFAULT:
-        if key not in configuracion:
-            configuracion[key] = CONFIGURACION_DEFAULT[key]
-
-    return configuracion
+    for clave in CONFIGURACION_DEFAULT:
+        if clave not in configuracion:
+            constantes.configuracion[clave] = CONFIGURACION_DEFAULT[clave]
     
 def smart_cast(value):
     """Joaquin: Infiere el tipo de dato a partir de un string y realiza la conversion correspondiente"""
