c276bf31903b893c6d74d6a1d289808a2b3cea50
Joaquin <jopiodz00@gmail.com>
2021-12-10 18:27:15 -0300

    Ventana: Historial de Partidas

---
+++ interfaz_partidas.py
@@ -0,0 +1,104 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+def crear_raiz():
+    """
+    Joaquin: crea el contexto de la ventana.
+    """
+    # create root window
+    root = tk.Tk()
+    root.title('Memotest: Historial de Partidas')
+    root.geometry('1080x300')
+
+    # configuracion del grid
+    root.rowconfigure(0, weight=1)
+    root.columnconfigure(0, weight=1)
+
+    return root
+
+def construir_treeview(raiz):
+    """
+    Joaquin: crea el contexto de TreeView y setea las columnas que vamos a usar.
+    """
+    # declaramos las columnas que vamos a usar
+    columnas = ('fecha_partida', 'hora_finalizacion', 'nombre_jugador', 'aciertos', 'intentos')
+
+    tree = ttk.Treeview(raiz, columns=columnas, show="headings")
+    
+    # columna "Fecha"
+    tree.heading('fecha_partida', text='Fecha')
+    tree.column(column='fecha_partida', width=50, anchor=tk.CENTER)
+    
+    # columna "Hora"
+    tree.heading('hora_finalizacion', text='Hora')
+    tree.column(column='hora_finalizacion', width=50, anchor=tk.CENTER)
+
+    # columna "Ganador"
+    tree.heading('nombre_jugador', text='Ganador')
+    tree.column(column='nombre_jugador', width=50, anchor=tk.CENTER)
+
+    # columna "Intentos"
+    tree.heading('intentos', text='Intentos')
+    tree.column(column='intentos', width=50, anchor=tk.CENTER)
+
+    # columna "Puntos"
+    tree.heading('aciertos', text='Puntos')
+    tree.column(column='aciertos', width=50, anchor=tk.CENTER)
+
+    tree.grid(row=0, column=0, sticky='nsew')
+    
+    return tree
+
+def mostrar_ventana():
+    """
+    Joaquin: interfaz que muestra un historial de las partidas registradas.
+    """
+    raiz = crear_raiz()
+    
+    # vista de arbol de datos
+    tree = construir_treeview(raiz)
+
+    # barra de desplazamiento
+    scrollbar = ttk.Scrollbar(raiz, orient=tk.VERTICAL, command=tree.yview)
+    tree.configure(yscroll=scrollbar.set)
+    scrollbar.grid(row=0, column=1, sticky='ns')
+
+    # llenamos la tabla con la información de las partidas
+    obtener_partidas(tree)
+
+    # muestro la ventana
+    raiz.mainloop()
+
+
+def leer_archivo(archivo):
+    """
+    Rodrigo: lee un archivo linea a linea
+    """
+    linea = archivo.readline()
+    linea = linea.rstrip('\n')
+
+    if linea:
+        linea_procesada = linea.split(',')
+    else:
+        linea_procesada = None
+
+    return linea_procesada
+
+def obtener_partidas(tree):
+    """
+    Joaquin: obtiene las partidas registradas en la base de datos y las agrega al TreeView
+    """
+    archivo = open('partidas.csv', 'r')
+    partida = leer_archivo(archivo)
+
+    while partida:
+        # agregamos los datos de la linea a la lista
+        tree.insert('', tk.END, values=partida)
+
+        # leo la siguiente linea
+        partida = leer_archivo(archivo)
+
+    archivo.close()
+
+mostrar_ventana()
